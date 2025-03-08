341a6ee4ed28b46275970494c332b42443cc44a9
novice000 <maxwellefemena@gmail.com>
2025-03-08 14:25:48 +0100

    added error and waring for wrong endpoint and/method

--- convert/urls.py
+++ convert/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("process-image/", views.ImageProc.as_view(), name="image_proc"),
-    paht("index/", views.index, name="index")
+    path("index/", views.index, name="index")
 ]
